7a720e9c4b57aa7471b483fe185f63a639a2ebd6
Caleb Carvalho <caleb.carvalho.ext@siemens.com>
2021-07-30 02:56:48 +0200

    fix compiling

--- app/controllers/api/EvaluationRestController.scala
+++ app/controllers/api/EvaluationRestController.scala
@@ -21,66 +21,31 @@ extends AbstractController(cc) {
   implicit val evalListJson = Json.format[Evaluation]
   implicit val evalJson = Json.format[Summary]
   
-  def foo(value: Any, params: Map[String, String]) = value match {
-    case Some("mostSpeaches") => getField(params) 
-    case default => None
-  }
-  def getField(implicit params: Map[String, String]) = {
-    println("gettting most field")
-    val date = params.get("date").getOrElse(null)
-    if(date!=null){
-      date
-    }
-  }
-
-  def process(implicit params: Map[String, String]): Future[Result] = {
-    val date = params.get("date").getOrElse(null)
-    if(date!=null){
-      print("ddddddddddddd")
-    }
-    evalService.getMost(LocalDate.parse(date))map {item=>
-    Ok("ok") 
-    }
-  }
   def getAll() = Action.async { implicit request: Request[AnyContent] =>
-      val params = request.queryString.map { case (k,v) => k -> v.mkString }
-      val query=params.get("query")
-      val rs=query match {
-          case Some("mostSpeaches")=> process(params)
-          case Some("mostSecurity")=> process(params)
-          case Some("leastWordy")=> process(params)
-          case default=> "Nope"
-       }  
-      var date = foo(query, params)
-      val b = date.asInstanceOf[String]
-      evalService.getMost(LocalDate.parse(b))map {item=>
-      //Ok(Json.toJson(Summary(item.get.speaker)))
-      }
-      evalService.listAllItems map { items =>
+        val params = request.queryString.map { case (k,v) => k -> v.mkString }
+        val query=params.get("query")
+        
+        evalService.listAllItems map { items =>
+        
+        //val mostSpeaches = ll.groupBy(identity).mapValues(_.size).maxBy(_._2)
+        //val mostSecurity = items.groupBy(l => l.subject).map(t => (t._1, t._2.length))
 
-        val mostSpeaches = items.groupBy(_.speaker).values.map(_.maxBy(_.words)).toList
+        val mostSecurity = items.groupBy(_.subject).values.map(_.maxBy(_.words)).filter(_.subject=="Internal Security").headOption.get
+        val mostSpeaches = items.groupBy(_.subject).values.map(_.maxBy(_.words)).filter(_.date=="2013").headOption.getOrElse(null)
+        //val mostSpeaches = items.groupBy(_.speaker).mapValues(_.maxBy(_.worwordsds))
+        //val max_food = items.groupBy(record => record.words.toInt)
+        //val allV = items.collect{ case Evaluation(id,_,_,_,words) => words }.sum
 
-        val mostSecurity = items.groupBy(l => l.subject).map(t => (t._1, t._2.length))
-
-        val mostSecurity = items.groupBy(_.speaker).values.map(_.maxBy(_.subject)).filter(_.subject=="Internal Security")
-        //val mostSpeaches = items.groupBy(_.speaker).mapValues(_.maxBy(_.words))
-
-        //print(items)
         println("--------------")
         println(mostSpeaches)
         println("sec")
-        print(mostSecurity)
-        println("xxx" + mostSpeachesX)
+   //     print(mostSecurity)
+        println("-----------")
+      ////  println(ll)
     
-       Ok(Json.toJson(Summary()))
+       Ok(Json.toJson(Summary(mostSecurity=mostSecurity.speaker)))
      }
   }
   
-   def options(id: Option[Long], name: Option[String], transaction: Option[Int]) = Action {
-      log(name.get)
-      log(""+id.get)
-      Ok("OK")
-  }
-  def log(message: String, level: String = "INFO") = println(s"$level: $message")
 }
 
